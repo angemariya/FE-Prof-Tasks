d855508fdcbd86e21494be994e5a5711e563e76b
Maria Angelova <angelova.m@hotmail.com>
2023-02-13 14:19:02 +0100

    upload files

---
+++ Lesson 1/script.js
@@ -0,0 +1,38 @@
+//1 цикл
+/*
+for(let i=0; i<=9; i++) {
+    console.log(i);
+}
+
+
+// Написать цикл который выводит в консоль только положительные числа
+
+
+for(let i=0; i<numbers.length; i++) {
+    if (numbers[i] < 0) {
+        numbers[i] *= -1;
+    }
+
+    console.log(numbers[i])
+}
+
+
+
+const numbers = [12, 4, -32, 5, -37, -32, 5, 23];
+
+for(let i=0; i<numbers.length; i++) {
+if (numbers[i] % 2 === 0) {
+    console.log (numbers[i]);
+    }
+}
+*/
+/*
+
+Создайте блок в html c картинкой и кнопку. 
+При нажатии на кнопку блок с картинкой должен получать 
+border-radius 50px и overflow hidden. При нажатии второй 
+раз на кнопку стили должны убираться и возвращаться к 
+исходным. Реализуйте через classList.toggle()
+
+*/
+
