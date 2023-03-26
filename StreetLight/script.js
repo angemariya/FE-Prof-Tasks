c85f63ff07b2719d160bec0a6aaa2dd605904f9e
Maria Angelova <angelova.m@hotmail.com>
2023-03-26 22:06:50 +0200

    Add styles

--- StreetLight/script.js
+++ StreetLight/script.js
@@ -1,34 +1,46 @@
+// <Model>
+
 const store = {
-    selectedCircle: null,
+    selectedCircle: null, // "red", "yellow", "green"
 }
 
 const setSelectedCircle = (circleName) => {
     store.selectedCircle = circleName;
+    onChange();
 }
 
-const container = document.querySelector('#container');
+// </Model>
+
+// <View>
+const container = document.getElementById("container");
 const draw = () => {
-
-    const redCircle = document.createElement('div');
-
+    const redCircle = document.createElement("div");
     redCircle.classList.add("circle");
-    redCircle.addEventListener("click", ()=>{
+    redCircle.addEventListener("click", () => {
         onCircleClick("red");
-    })
+    });
 
-    const yellowCircle = document.createElement('div');
-
-    redCircle.classList.add("circle");
-    redCircle.addEventListener("click", ()=>{
+    const yellowCircle = document.createElement("div");
+    yellowCircle.classList.add("circle");
+    yellowCircle.addEventListener("click", () => {
         onCircleClick("yellow");
     })
 
-    const greenCircle = document.createElement('div');
-
-    redCircle.classList.add("circle");
-    redCircle.addEventListener("click", ()=>{
+    const greenCircle = document.createElement("div");
+    greenCircle.classList.add("circle");
+    greenCircle.addEventListener("click", () => {
         onCircleClick("green");
-    })
+    });
+
+    if (store.selectedCircle === "red") {
+        redCircle.classList.add("red");
+    }
+    if (store.selectedCircle === "yellow") {
+        yellowCircle.classList.add("yellow");
+    }
+    if (store.selectedCircle === "green") {
+        greenCircle.classList.add("green");
+    }
 
     container.innerHTML = "";
     container.append(redCircle, yellowCircle, greenCircle);
@@ -36,6 +48,15 @@ const draw = () => {
 
 draw();
 
+// </View>
+
+// <Controller>
+
 const onCircleClick = (circleName) => {
-    setSelectedCircle(circleName)
+    setSelectedCircle(circleName);
 }
+const onChange = () => {
+    draw();
+}
+
+// </Controller>
