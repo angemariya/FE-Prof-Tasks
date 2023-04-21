33207abb1143676ead1ffdcc173ed4e3d33d529f
Maria Angelova <angelova.m@hotmail.com>
2023-04-21 20:56:15 +0200

    Lesson 13/Fetch/Fe tasks

--- Lesson 13/script.js
+++ Lesson 13/script.js
@@ -1,36 +1,17 @@
-const users = [
-    {id: 1, name: 'Анатолий', age: 34, gender: 'm'},
-    {id: 2, name: 'Светлана', age: 25, gender: 'f'},
-    {id: 3, name: 'Степан', age: 53, gender: 'm'},
-    {id: 4, name: 'Ольга', age: 63, gender: 'f'},
-    {id: 5, name: 'Антон', age: 39, gender: 'm'}
-]
-
-//users.forEach(el => console.log(el.name));
-//users.forEach(({name})=> console.log(name));
-
-//const arrMale = users.filter(el => el.gender === 'm');
-//const arrdesctr = users.filter({gender} => gender === "m")
-//console.log(arrMale);
-//console.log(users.filter(el => el.gender === "m").map(el=> el.name))
-
-//console.log(users.reduce((acc, el) => acc + el.age, 0))
-
-const root = document.querySelector('#root');
-const createCard = (name, age, gender) => {
-    const container = document.createElement("div");
-    const nameP = document.createElement("p");
-    const ageP = document.createElement("p");
-    nameP.innerHTML = name;
-    ageP.innerHTML = age;
-    gender === "m" ? container.classList.add("blue") : container.classList.add("rosa");
-
-    container.append(nameP, ageP);
-    root.append(container);
+const get_req = async (id) => {
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const {userId, title, body} = await resp.json();
+    const respUser = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const {name, username} = await respUser.json();
+    console.log(userId, id, name, username, title, body);
 }
 
-const render = (arr) => {
-    arr.forEach(el => createCard(el.name, el.age, el.gender))
+
+const printArr = async () => {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await resp.json();
+    const titles = data.map(({title}) => title);
+    console.log(titles)
 }
 
-render(users)
+printArr();
